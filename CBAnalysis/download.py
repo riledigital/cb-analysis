9c70da367829e2531840df397b737061b27c42af
Ri Le <rl2999@columbia.edu>
2023-02-19 16:05:24 -0500

    Fix date range download

--- CBAnalysis/download.py
+++ CBAnalysis/download.py
@@ -1,52 +1,72 @@
+from pprint import pprint
+
 import datetime
-import operator
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import re
 
+
 def download_file_list():
-    URL = 'https://s3.amazonaws.com/tripdata'
+    URL = "https://s3.amazonaws.com/tripdata"
     with urlopen(URL) as response:
-      html_doc = response.read()
-      soup = BeautifulSoup(html_doc, 'html.parser')
-      tags = soup.find_all('key')
-      filenames = [tag.string for tag in tags]
-      return filenames
-      # print(soup.prettify())
+        html_doc = response.read()
+        soup = BeautifulSoup(html_doc, "html.parser")
+        tags = soup.find_all("key")
+        filenames = [tag.string for tag in tags]
+        return filenames
+        # print(soup.prettify())
+
 
 def parse_date_from_filename(filename):
-   match = re.search('\d{6}', filename)
-   if not match:
-      return
-   date = match[0]
-   year = date[:4]
-   month = date[4:6]
-   return {"year": year, "month": month}
-  
+    match = re.search("\d{6}", filename)
+    if not match:
+        return
+    date = match[0]
+    year = date[:4]
+    month = date[4:6]
+    return {"year": year, "month": month}
+
+
 def parse_filename_to_metadata(filename):
-  if not filename.endswith('zip'):
-    return
-  record = { "date": parse_date_from_filename(filename), "filename": filename }
-  return record  
+    if not filename.endswith("zip"):
+        return
+    record = {"date": parse_date_from_filename(filename), "filename": filename}
+    return record
+
 
 def parse_filenames_to_metadata(filenames):
-  result = []
-  for filename in filenames:
-    record = parse_filename_to_metadata(filename)
-    result.append(record)
-  return result
+    result = []
+    for filename in filenames:
+        record = parse_filename_to_metadata(filename)
+        result.append(record)
+    return result
+
 
 def parse_meta_date(meta):
-  return datetime.datetime(meta['year'], meta['month'], 1)
+    return datetime.datetime(int(meta["year"]), int(meta["month"]), 1)
+
 
 def filter_range(files, start_date, end_date):
-  start = parse_meta_date(start_date)
-  end = parse_meta_date(end_date)
-  return [*filter(lambda file: start <= parse_meta_date(file['date']) and end >= parse_meta_date(file['date']))]
-  
+    start = parse_meta_date(start_date)
+    end = parse_meta_date(end_date)
 
-def main(): 
-  filenames = download_file_list()
-  all_downloads = parse_filenames_to_metadata(filenames)
-  filtered_downloads = filter_range(all_downloads, { 'year': '2020', 'month': '01' }, { 'year': '2021', 'month': '12' })
-  print(filtered_downloads)
+    def compare(file: dict):
+        if file is None:
+            return False
+        date = parse_meta_date(file["date"])
+        return (start <= date) and (end >= date)
+
+    return list(filter(compare, files))
+
+
+def download_date_range(
+    start={"year": "2020", "month": "01"}, end={"year": "2020", "month": "02"}
+):
+    filenames = download_file_list()
+    all_downloads = parse_filenames_to_metadata(filenames)
+    filtered_downloads = filter_range(all_downloads, start, end)
+    # pprint(filtered_downloads)
+    return filtered_downloads
+
+
+
--- CBAnalysis/test_download.py
+++ CBAnalysis/test_download.py
@@ -1,12 +1,16 @@
 from .download import parse_date_from_filename, compare_date
 
+
 def test_parse_date_from_filename():
-  assert parse_date_from_filename("citibike-tripdata.csv.zip") is None
-  assert parse_date_from_filename("201912-citibike-tripdata.csv.zip") == { "year" : "2019", "month": "12"}  
-  
+    assert parse_date_from_filename("citibike-tripdata.csv.zip") is None
+    assert parse_date_from_filename("201912-citibike-tripdata.csv.zip") == {
+        "year": "2019",
+        "month": "12",
+    }
+
+
 def parse_filename_to_metadata():
-  test_filename = "201912-citibike-tripdata.csv.zip"
-  result = parse_filename_to_metadata(test_filename)
-  expected ={"date": {"year": "2019", "month": "12"}, "filename": test_filename}
-  assert result is expected
-  
+    test_filename = "201912-citibike-tripdata.csv.zip"
+    result = parse_filename_to_metadata(test_filename)
+    expected = {"date": {"year": "2019", "month": "12"}, "filename": test_filename}
+    assert result is expected
